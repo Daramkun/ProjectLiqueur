246971c651d31f2c31374c95ec33966b322b561a
Jin Jae-yeon <daramkun@daram.pe.kr>
2013-09-16 01:10:56 +0900

    Add Launcher for Android Platform Framework

---
+++ Daramkun.Liqueur.Platforms.Android/Platforms/Launcher.cs
@@ -0,0 +1,89 @@
+using System;
+using Daramkun.Liqueur.Platforms;
+using System.Net.NetworkInformation;
+using Daramkun.Liqueur.Graphics;
+using Daramkun.Liqueur.Audio;
+using OpenTK.Graphics;
+
+namespace Daramkun.Liqueur
+{
+	public class Launcher : ILauncher
+	{
+		public bool IsInitialized { get; private set; }
+
+		public PlatformInformation PlatformInformation
+		{
+			get
+			{
+				return new PlatformInformation ()
+				{
+					UserName = Environment.UserName,
+					PlatformType = PlatformType.Android,
+					PlatformVersion = Environment.OSVersion.Version,
+					MachineUniqueIdentifier = NetworkInterface.GetAllNetworkInterfaces () [ 0 ].GetPhysicalAddress ().ToString ()
+				};
+			}
+		}
+
+		public Launcher ()
+		{
+			IsInitialized = false;
+		}
+
+		public void LauncherInitialize ( out IWindow window, out IGraphicsDevice graphicsDevice, out IAudioDevice audioDevice )
+		{
+			window = new Window ();
+			graphicsDevice = new GraphicsDevice ( window );
+			audioDevice = new AudioDevice ( window );
+
+			IsInitialized = true;
+		}
+
+		public void LauncherRun ( LauncherArgument args )
+		{
+			GraphicsContext.ShareContexts = true;
+			OpenTK.Platform.Android.AndroidGameView gameView = LiqueurSystem.Window.Handle as OpenTK.Platform.Android.AndroidGameView;
+
+			gameView.Resize += ( object sender, EventArgs e ) => { args.Resize (); };
+			gameView.FocusedChanged += ( object sender, EventArgs e ) =>
+			{
+				if ( gameView.Focused )
+				{
+					if ( LiqueurSystem.GraphicsDevice.FullscreenMode )
+						OpenTK.DisplayDevice.Default.ChangeResolution ( OpenTK.DisplayDevice.Default.SelectResolution ( ( int )
+						                                                                                               LiqueurSystem.GraphicsDevice.ScreenSize.X, ( int ) LiqueurSystem.GraphicsDevice.ScreenSize.Y, 32, 60 ) );
+					args.Activated ();
+				}
+				else
+				{
+					args.Deactivated ();
+					OpenTK.DisplayDevice.Default.RestoreResolution ();
+					OpenTK.DisplayDevice.Default.ChangeResolution ( ( LiqueurSystem.GraphicsDevice as GraphicsDevice ).originalResolution );
+				}
+			};
+			gameView.Load += ( object sender, EventArgs e ) =>
+			{
+				LiqueurSystem.Window.Title = "Project Liqueur";
+				args.Initialize ();
+			};
+			gameView.UpdateFrame += ( object sender, OpenTK.FrameEventArgs e ) =>
+			{
+				args.UpdateLogic ();
+			};
+			gameView.RenderFrame += ( object sender, OpenTK.FrameEventArgs e ) =>
+			{
+				args.DrawLogic ();
+			};
+			gameView.Run ();
+		}
+
+		public void LauncherFinalize ( IWindow window, IGraphicsDevice graphicsDevice, IAudioDevice audioDevice )
+		{
+			if ( audioDevice != null )
+				audioDevice.Dispose ();
+			graphicsDevice.Dispose ();
+			window.Dispose ();
+		}
+	}
+}
+
