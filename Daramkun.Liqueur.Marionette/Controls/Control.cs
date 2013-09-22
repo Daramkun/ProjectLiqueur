07c6273cca76b1b994fac54ccd4ac7fe342fd332
Jin Jae-yeon <daramkun@daram.pe.kr>
2013-09-22 21:39:01 +0900

    Add Marionette framework and Box2D framework

---
+++ Daramkun.Liqueur.Marionette/Controls/Control.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Daramkun.Liqueur.Nodes;
+
+namespace Daramkun.Liqueur.Marionette.Controls
+{
+	public class Control : IDisposable
+	{
+		List<Control> children = new List<Control> ();
+
+		public Control Parent { get; private set; }
+		public IEnumerable<Control> Children { get { return children; } }
+
+		~Control ()
+		{
+			Dispose ( false );
+		}
+
+		protected virtual void Dispose ( bool isDisposing )
+		{
+		
+		}
+
+		public void Dispose ()
+		{
+			Dispose ( true );
+			GC.SuppressFinalize ( this );
+		}
+	}
+}
---
+++ Daramkun.Liqueur.Marionette/Marionette.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Daramkun.Liqueur.Nodes;
+
+namespace Daramkun.Liqueur.Marionette
+{
+	public class Marionette : Node
+	{
+
+	}
+}
