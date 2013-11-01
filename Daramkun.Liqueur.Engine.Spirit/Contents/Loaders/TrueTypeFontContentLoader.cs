a5a3b9ebfc3fbd09b83ae68f1b701e9ae7b71aef
Jin Jae-yeon <daramkun@daram.pe.kr>
2013-11-01 23:00:22 +0900

    Add TTF Content Loader

---
+++ Daramkun.Liqueur.Engine.Spirit/Contents/Loaders/TrueTypeFontContentLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Daramkun.Liqueur.Spirit.Graphics;
+
+namespace Daramkun.Liqueur.Contents.Loaders
+{
+	public class TrueTypeFontContentLoader : IContentLoader
+	{
+		public Type ContentType { get { return typeof ( TrueTypeFont ); } }
+
+		public IEnumerable<string> FileExtensions { get { yield return "LSF"; } }
+
+		public bool IsSelfStreamDispose { get { return false; } }
+
+		public object Load ( Stream stream, params object [] args )
+		{
+			return new TrueTypeFont ( stream, ( int ) args [ 0 ] );
+		}
+	}
+}
