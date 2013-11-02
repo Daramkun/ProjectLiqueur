cea21a519fc3510e27e572d21337b30b5d244c13
Jin Jae-yeon <daramkun@daram.pe.kr>
2013-11-02 16:38:10 +0900

    Tools Integrated to Lemon Project

---
+++ Daramkun.Liqueur.Tools.Lemon/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Daramkun.Liqueur.Tools.Lemon.Tools;
+
+namespace Daramkun.Liqueur.Tools.Lemon
+{
+	class Program
+	{
+		static void Main ( string [] args )
+		{
+			if ( args [ 0 ] == "-h" )
+			{
+				Console.WriteLine ( "USECASE:" );
+				Console.WriteLine ( "  lemon.exe -[lsf|res|stt] [args]" );
+				Console.WriteLine ( "    lsf: Liqueur Sprite Font Generator" );
+				Console.WriteLine ( "    res: Resource Table Editor" );
+				Console.WriteLine ( "    stt: String Table Editor" );
+			}
+			else
+			{
+				List<string> arguments = new List<string> ( args );
+				string tool = arguments [ 0 ];
+				arguments.RemoveAt ( 0 );
+				switch ( tool )
+				{
+					case "-lsf":
+						new LiqueurSpriteFontGenerator ().Run ( arguments.ToArray () );
+						break;
+					case "-res":
+						new ResourceTableEditor ().Run ( arguments.ToArray () );
+						break;
+					case "-stt":
+						new StringTableEditor ().Run ( arguments.ToArray () );
+						break;
+				}
+			}
+		}
+	}
+}
---
+++ Daramkun.Liqueur.Tools.Lemon/Tools/ResourceTableEditor.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daramkun.Liqueur.Tools.Lemon.Tools
+{
+	class ResourceTableEditor : ITool
+	{
+		public void Run ( params string [] args )
+		{
+			if ( args.Length == 0 || args [ 0 ] == "-h" )
+			{
+				Console.WriteLine ( "USECASE:" );
+				Console.WriteLine ( "  remon.exe -res filename" );
+			}
+			else
+			{
+				CultureInfo cultureInfo = CultureInfo.CurrentCulture;
+				using ( FileStream stream = new FileStream ( args [ 0 ], FileMode.OpenOrCreate, FileAccess.ReadWrite ) )
+				{
+					ZipArchive archive = new ZipArchive ( stream, ZipArchiveMode.Update, false, Encoding.UTF8 );
+
+					while ( true )
+					{
+						Console.WriteLine ( "==========================================" );
+						Console.WriteLine ( "CURRENT CULTURE: {0}", cultureInfo );
+						Console.WriteLine ( "MENU:" );
+						Console.WriteLine ( "  1. Add resource" );
+						Console.WriteLine ( "  2. Remove resource" );
+						Console.WriteLine ( "  3. Show resources of current culture" );
+						Console.WriteLine ( "  4. Change current culture" );
+						Console.WriteLine ( "  5. Quit without save" );
+						Console.WriteLine ( "  else. save and quit" );
+						Console.Write ( ">" );
+
+						int menuSelect;
+						if ( !int.TryParse ( Console.ReadLine (), out menuSelect ) )
+							menuSelect = 6;
+
+						Console.WriteLine ( "-----------------------------------------" );
+
+						switch ( menuSelect )
+						{
+							case 1:
+								{
+									string dir = Environment.CurrentDirectory;
+									while ( true )
+									{
+										Console.Write ( "CMD>" );
+										string cmd = Console.ReadLine ();
+										if ( cmd == "dir" || cmd == "ls" )
+										{
+											foreach ( string d in Directory.GetDirectories ( dir ) )
+												Console.WriteLine ( "[D] {0}", Path.GetFileName ( d ) );
+											foreach ( string f in Directory.GetFiles ( dir ) )
+												Console.WriteLine ( "[F] {0}", Path.GetFileName ( f ) );
+										}
+										else if ( cmd == "pwd" )
+										{
+											Console.WriteLine ( "PATH>{0}", dir );
+										}
+										else if ( cmd.Substring ( 0, 2 ) == "cd" )
+										{
+											string folder = cmd.Substring ( 3, cmd.Length - 3 );
+											if ( folder.Trim () == ".." )
+											{
+												List<string> temp = new List<string> ( dir.Split ( Path.DirectorySeparatorChar ) );
+												temp [ 0 ] += Path.DirectorySeparatorChar;
+												temp.RemoveAt ( temp.Count - 1 );
+												dir = Path.Combine ( temp.ToArray () );
+											}
+											else
+												dir = Path.Combine ( dir, folder.Trim () );
+											Console.WriteLine ( "PATH>{0}", dir );
+										}
+										else if ( cmd.Substring ( 0, 3 ) == "add" )
+										{
+											string filename = cmd.Substring ( 4, cmd.Length - 4 );
+											byte [] data = File.ReadAllBytes ( Path.Combine ( dir, filename ) );
+											ZipArchiveEntry entry = archive.CreateEntry ( Path.Combine ( cultureInfo.Name, filename ) );
+											using ( Stream s = entry.Open () )
+											{
+												s.Write ( data, 0, data.Length );
+											}
+											break;
+										}
+									}
+								}
+								break;
+							case 2:
+								{
+									Console.Write ( "FILENAME>" );
+									string cmd = Console.ReadLine ();
+
+									ZipArchiveEntry entry = archive.GetEntry ( Path.Combine ( cultureInfo.Name, cmd ) );
+									entry.Delete ();
+								}
+								break;
+							case 3:
+								{
+									foreach ( ZipArchiveEntry entry in archive.Entries )
+										if ( Path.GetDirectoryName ( entry.FullName ) == cultureInfo.Name )
+											Console.WriteLine ( entry.Name );
+								}
+								break;
+							case 4:
+								{
+									Console.Write ( "CULTURE>" );
+									cultureInfo = new CultureInfo ( Console.ReadLine () );
+								}
+								break;
+							case 5:
+								return;
+							default:
+								return;
+						}
+					}
+				}
+			}
+		}
+	}
+}
