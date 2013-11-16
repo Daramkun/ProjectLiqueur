c5345e349892cd42aad40c99bba8334764f795ee
Jin Jae-yeon <daramkun@daram.pe.kr>
2013-11-16 15:48:24 +0900

    Missing commit

--- Test.Windows.CSharp/Program.cs
+++ Test.Windows.CSharp/Program.cs
@@ -32,10 +32,10 @@ namespace Test.Windows.CSharp
 			//try
 			//{
 				LiqueurSystem.Run ( new Launcher ( true ),
-					//new Test.Game.Dodge.Container (),
+					new Test.Game.Dodge.Container (),
 					//new Test.Game.PerformanceTester.Container (),
 					//new Test.Game.InfoViewer.Container (),
-					new Test.Game.PlaySound.Container (),
+					//new Test.Game.PlaySound.Container (),
 					() =>
 					{
 						LiqueurSystem.Keyboard = new Keyboard ( LiqueurSystem.Window );
